ee1a98deef8dadc097cdccc651f70928c8a412c7
gaveenkith2 <gaveen0342242323@gmail.com>
2018-10-05 07:55:12 -0700

    register page editmap with synchronized

--- app/controllers/registerPageController.js
+++ app/controllers/registerPageController.js
@@ -1,13 +1,18 @@
 app.controller('registerPageController', function ($scope, $http) {
     $scope.show_map = false;
-    $scope.test="test";
+    $scope.obj = {
+        prop1:"",
+        prop2:""
+    };
     $http.post("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDbMnH8NLQ-689cNuQyVqrLHLhzAYzu0g4")
         .then(function (response) {
             $scope.myWelcome = response.data;
-            $scope.lat = $scope.myWelcome.location.lat;
-            $scope.lon = $scope.myWelcome.location.lng;
-            $scope.show_map = true;
-
+            $scope.obj.prop1 = $scope.myWelcome.location.lat;
+            $scope.obj.prop2 = $scope.myWelcome.location.lng;
         });
-
+    $scope.editMapClick = function () {
+        if ($scope.obj.prop1 != null && $scope.obj.prop2) {
+            $scope.show_map = true;
+        }
+    }
 });
--- app/directives/editMapDirective.js
+++ app/directives/editMapDirective.js
@@ -1,21 +1,36 @@
 app.directive('mapDir', function () {
     return {
         restrict: 'E',
-        scope:{
-            location:"="
+        scope: {
+            location: "="
         },
-        link: function (scope, element,attr) {
-            console.log(scope.location);
+        link: function (scope, element, attrs) {
+            scope.changeValue = function (txt) {
+                scope.obj.prop = txt;
+                alert(scope.obj.prop);
+            }
             var mapOptions = {
                 zoom: 7,
                 center: new google.maps.LatLng(7.0008, 80.7733)
             };
             var map = new google.maps.Map(element[0], mapOptions);
-                var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(scope.location.location.lat, scope.location.location.lng),
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(scope.location.prop1, scope.location.prop2),
+                animation: google.maps.Animation.BOUNCE
+            });
+            marker.setMap(map);
+            google.maps.event.addListener(map, 'click', function (event) {
+                //alert(event.latLng.lat());
+                marker.setMap(null);
+                scope.location.prop1 = event.latLng.lat();
+                scope.location.prop2 = event.latLng.lng();
+                marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(scope.location.prop1, scope.location.prop2),
                     animation: google.maps.Animation.BOUNCE
                 });
                 marker.setMap(map);
+            });
+
         }
     };
 });
