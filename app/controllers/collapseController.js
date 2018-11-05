54330e5a3c881a9c6ee1b520c461078810e08b07
gaveenkith2 <gaveen0342242323@gmail.com>
2018-11-05 07:38:53 -0800

    add navigation menu (collapse)

---
+++ app/controllers/collapseController.js
@@ -0,0 +1,10 @@
+app.controller('collapseDemoCtrl', function ($scope) {
+    $scope.isCollapsedMod1 = true;
+    $scope.isCollapsedMod2 = true;
+    $scope.isCollapsedMod3 = true;
+    $scope.isCollapsedMod4 = true;
+    $scope.isCollapsedMod5 = true;
+    $scope.isCollapsedMod6 = true;
+    $scope.isCollapsedMod7 = true;
+    $scope.isCollapsedMod8 = true;
+});
